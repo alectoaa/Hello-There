b13b8836b3f8183bc0894d190a91b682c00a9adb
Alecto <kemalsyedek@gmail.com>
2026-08-31 13:55:35 +0300

    Improve messages in install.sh script
    Updated error messages and installation progress messages for clarity.

--- install.sh
+++ install.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-# Root (sudo) yetkisi kontrolü
+
 if [ "$EUID" -ne 0 ]; then
-  echo "[-] Please execute this with sudo: sudo bash install.sh"
+  echo "[-] Please run this script with sudo: sudo bash install.sh"
   exit 1
 fi
 
-echo "[+] Hello-There installing..."
+echo "[+] Hello-There is processing...."
 
 cp hellothere.py /usr/local/bin/hellothere
+
+
 chmod +x /usr/local/bin/hellothere
+
+if command -v dos2unix &> /dev/null; then
+    dos2unix /usr/local/bin/hellothere
+fi
 
 echo "[+] Installation complete! You can now run it by typing 'hellothere' from anywhere."
